746a07471ca8a461751f858c0a19c338dc87a6a4
DiuDiu <A8206115@g.pccu.edu.tw>
2022-03-08 23:34:49 +0800

    node fixed

--- Algorithm_with_SQL/graph.py
+++ Algorithm_with_SQL/graph.py
@@ -8,111 +8,8 @@ import matplotlib.pyplot as plt  # 畫圖顯示
 class recommend_Graph:
     def __init__(self, city):
         self.node = []  # 節點存放處（物件 node )
-        self.edge = []  # 邊的存放處 (權重 {nodeA, nodeB} )
+        self.edge = []  # 邊的存放處 (權重 {nodeA, nodeB, nodeC} )
         self.weather_info = weather_information_API(city)  # 取得城市當日天氣資訊：溫度、濕度、最高溫、最低溫
-
-    def addNode(self, newNode):
-
-        # 將所有分類不同的上下衣物連線，賦予權重
-        for n in self.node:
-            self.addEdge(newNode, n)
-
-        self.node.append(newNode)  # 將節點新增至陣列當中
-
-    def printNode(self):
-        for n in self.node:
-            print('位置:', n.position, '| 顏色:', n.color, '| 分類:', n.category)
-
-    def printEdge(self):
-        for e in self.edge:
-            print(e[0], e[1][0].position, e[1][1].position)
-
-    # 配分（根據穿搭的顏色進行搭配）（後續新增）
-    def addEdge(self, nodeA, nodeB):
-
-        # 先判斷是不是為不同類別, 且不為 other類別
-        if nodeA.category == nodeB.category:
-            return
-
-        # !#################### 給予分數（Not Done）
-        w = self.Allocation(nodeA.color, nodeB.color)
-
-        # 連接邊
-        self.edge.append([w, [nodeA, nodeB]])
-
-    def Allocation(self, A_color, B_color):
-
-        ### 色彩配置 (同色系)
-
-        w = int(random.random() * 10)
-        return w
-
-    def combination(self):
-        combs = []
-        for e in self.edge:
-            total = e[0] + e[1][0].weather_score + e[1][1].weather_score
-            combs.append([total, [e[1][0].position, e[1][1].position]])
-            # print(e[0], e[1][0].position)
-
-        combs.sort(reverse=True)
-
-        return combs
-
-    def draw(self):
-        G = nx.Graph()
-        for n in self.node:
-            G.add_node(str(n.position))
-
-        edgesList = []
-
-        for e in self.edge:
-            edgesList.append((str(e[1][0].position), str(e[1][1].position)))
-
-        G.add_edges_from(edgesList)
-
-        nx.draw(G, with_labels=True, node_color='y')
-        plt.show()
-
-    def refresh_allWeatherScore(self):
-        for n in self.node:
-            n.refresh_WS(self.weather_info.getWeather())
-            # n.refresh_WS([30])
-
-    def otherRecommend(self, otherList, nowScore):
-
-        bestScore = 40
-        diff = bestScore - nowScore
-        bias = 0
-        min = 999
-        position = -1
-        for other in otherList:
-            if other[0] == 'coat':
-                bias = 5
-            elif other[0] == 'downCoat':
-                bias = 8
-
-            tempScore = abs(diff - bias * 4)
-            if tempScore < min:
-                min = tempScore
-                position = other[1]
-
-        return position
-
-    def systemRecommend(self):
-
-        combs = self.combination()
-        crud = nodeCRUD()
-        #!# 透過字典搜尋最適合的衣物
-        for comb in combs:
-            print(comb)
-            print(crud.queryDataByPosition(comb[1][0]), crud.queryDataByPosition(comb[1][1]))
-            if(comb[0] < 40):
-                print('建議推薦位置:', self.otherRecommend(self.otherList, comb[0]))
-            else:
-                print('暫時不需要額外增加衣物')
-
-        #!# 透過最高溫、最低溫和目前溫度的差距推薦衣物（正負十度）
-        return 0
 
 
 '''
@@ -120,7 +17,7 @@ class recommend_Graph:
 
 變數包含:
 節點:Node
-權重與邊 {w,(A,B)}:Edge
+權重與邊 {w,(A,B,C)}:Edge
 
 方法包含:
 新增節點:addNode (節點新增至陣列中, addEdge: 將所有上下衣物的邊連接)
--- Algorithm_with_SQL/node.py
+++ Algorithm_with_SQL/node.py
@@ -1,33 +1,53 @@
 
 
+from unittest import case
+
+
 class recommend_node:
 
-    # 初始化：位置、分類、天氣、天氣分數
-    def __init__(self, position, category, color, type, usageCounter , createTime, position):
+    # 初始化：位置、分類、顏色、種類、使用次數、創建時間、儲存位置
+    def __init__(self, position, category, color, type, usageCounter, createTime, photoPosition):
         self.position = position  # 面對存儲的位置（1, 2, 3, ...)
-        self.category = category  # (upper, lower）
+        self.category = category  # (upper, lower, other, outerwear）
         self.color = color  # 衣物的顏色
         self.type = type  # 衣物的種類
-        self.weather_score = 0  # 每日都會變更，利用function 抓取 中央氣象局 API 分數分數
-        self.usageCounter = usageCounter # 使用次數
-        self.createTime = createTime # 創建時間
-        
-        self.photoPosition = position
-        self.__comfortableTemp = 26 # 人體最適溫度
-        
+        # self.weather_score = 0  # 每日都會變更，利用function 抓取 中央氣象局 API 分數分數
+        self.usageCounter = usageCounter  # 使用次數
+        self.createTime = createTime  # 創建時間
+        self.photoPosition = photoPosition
+
         # self.style = style # 衣物的風格
-              
-    def refresh_WS(self, weather_info):
-        # weather_info 包含溫度、濕度、最高溫、最低溫、最高溫時間段、最低溫時間段
-        
-        
-        
-        return 0
-        
-    def setComfortableTemp(self, newTemp):
-        self.__comfortableTemp = newTemp
-          
-     
+
+    def refresh_WS(self):
+
+        # 上半身
+        if self.category == 'upper':
+            if self.type == 'short_TShirt':
+                return 1
+            elif self.type == 'long_sleeves':
+                return 2
+            elif self.type == 'sweater':
+                return 2
+
+        # 下半身
+        elif self.category == 'lower':
+            if self.type == 'short_pants':
+                return 1
+            elif self.type == 'long_skirt':
+                return 2
+            elif self.type == 'long_pants':
+                return 3
+            elif self.type == 'short_skirt':
+                return 1
+
+        # 其他
+        elif self.category == 'coat':
+            if self.type == 'thick_coat':
+                return 5
+            elif self.type == 'down_coat':
+                return 9
+            elif self.type == 'thin_coat':
+                return 3
 
 
 '''
@@ -47,9 +67,12 @@ class recommend_node:
 衣物資料庫包含:
 上:
 1. 短袖(1): short_TShirt
-2. 長袖(2): long_TShirt
-3. 毛衣(3): sweater
+2. 薄長袖(2): long_sleeves
+3. 針織毛衣(2): sweater
 
+上(可疊穿) outerwear:
+1. 厚毛衣(5): thick_sweater
+2. 帽T(4): cap_T
 
 下:
 1. 短褲(1): short_pants
@@ -58,8 +81,9 @@ class recommend_node:
 4. 短裙(0.5): short_skirt
 
 其他1:
-1. 外套(5): coat
-2. 羽絨衣(8): downCoat
+1. 厚外套(5): thick_coat
+2. 羽絨衣(9): down_coat
+3. 薄外套(3): thin_coat
 
 其他2:
 1. 發熱衣服(3): thermals
