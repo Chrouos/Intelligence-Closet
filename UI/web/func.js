1e4d5b8560ec0fc2b324ae7067787add3b7ae1ee
DiuDiu <A8206115@g.pccu.edu.tw>
2022-04-16 14:58:45 +0800

    (天氣) Python -> Javascript -> HTML

--- UI/web/func.js
+++ UI/web/func.js
@@ -2,7 +2,38 @@ async function get_weather(){
     
     var weather = await eel.weather_to_js()()  
     json = JSON.parse(weather);
-  
-    document.getElementById('weather_panel').textContent = json;
+
+    // document.getElementById("weather_temp").textContent = temp_temp; // 原始寫法
+
+    if(json.temp != -1) {
+        var tempStr = "現在溫度: " + json.temp;
+        document.getElementById("weather_temp").textContent = tempStr;
+    }
+
+    if(json.humd != -1) {
+       var humdStr = "濕度: " + json.humd;
+        document.getElementById("weather_humd").textContent = humdStr;
+    }
+
+    if(json.d_tx != -1) {
+    var dtxStr = "今日最高溫: " + json.d_tx;
+    document.getElementById("weather_d_tx").textContent = dtxStr;
+    }
+    
+    if(json.d_tn != -1) {
+    var dtnStr = "今日最高溫: " + json.d_tn;
+    document.getElementById("weather_d_tn").textContent = dtnStr;
+    }
+
+    if(json.d_txt != -1) {
+    var dtxtStr = "今日最高溫時間: " + json.d_txt;
+    document.getElementById("weather_d_txt").textContent = dtxtStr;
+    }
+
+    if(json.d_tnt != -1) {
+    var dtxtStr = "今日最高溫時間: " + json.d_tnt;
+    document.getElementById("weather_d_tnt").textContent = dtxtStr;
+    }
+
 }
 get_weather();
--- launcher.py
+++ launcher.py
@@ -6,17 +6,17 @@ def weather_to_js():    # 傳送天氣資訊
   
   we = weather_information_API("埔心") # 地點
   weather_list = we.getWeather() # 獲得陣列(6個資訊)
-  print(weather_list)
+  print("weather_list", weather_list)
   
   # 以下轉換成 JSON格式的字串
   weather_json = ""
-  # weather_json += '{"temp":' + weather_list[0]
-  # weather_json += ',"humd":' + weather_list[1]
-  # weather_json += ',"d_tx":' + weather_list[2]
-  # weather_json += ',"d_tn":' + weather_list[3]
-  # weather_json += ',"d_txt":' + weather_list[4]
-  # weather_json += ',"d_tnt":' + weather_list[5] + '}'
-  
+  weather_json += '{"temp":'   + str(weather_list[0])
+  weather_json += ',"humd":'   + str(weather_list[1])
+  weather_json += ',"d_tx":'   + str(weather_list[2])
+  weather_json += ',"d_tn":'   + str(weather_list[3])
+  weather_json += ',"d_txt":"' + str(weather_list[4]) + '"'
+  weather_json += ',"d_tnt":"' + str(weather_list[5])  + '"}'
+
   return weather_json
 
 eel.init('UI/web') # eel.init(網頁的資料夾)
