e17d8432fd57e5c8b90146d60001b662d60e4fc0
A8222668 <A8222668@g.pccu.edu.tw>
2022-06-29 18:02:24 +0800

    沒有使用到jQuery，但程式碼較冗長

--- UI/web/js/customize.js
+++ UI/web/js/customize.js
@@ -7,34 +7,53 @@ app.controller('myCtrl', function ($scope) {
 	upperCloth();
 	lowerCloth();
 
+	$scope.setUpper="has-background-info";
+	$scope.setUpper1="has-background-info";
+	$scope.setUpper2="has-background-info";
+
 	function upperCloth(){
-		$('.columns #upper').each( function(eq) {
-			$(this).off().click( function() {//當點擊上衣的任一件
-				//console.log(getUpperIndex);
-				$('.upHide').eq(getUpperIndex).hide();//上一件點選的衣服會從黃框變藍框
-				$('.upShow').eq(getUpperIndex).show();
-				window.setTimeout( function() {
-					$('.upShow').eq(eq).hide();//這次點選的的衣服會從藍框變黃框
-					$('.upHide').eq(eq).show();
-				});
-				getUpperIndex=eq;//抓取當前衣服的index
-			});
-		});
+		$scope.changeUpper=function(){
+			$scope.setUpper="has-background-warning";
+			$scope.setUpper1="has-background-info";
+			$scope.setUpper2="has-background-info";
+			getUpperIndex=0;
+		};
+		$scope.changeUpper1=function(){
+			$scope.setUpper="has-background-info";
+			$scope.setUpper1="has-background-warning";
+			$scope.setUpper2="has-background-info";
+			getUpperIndex=0;
+		};
+		$scope.changeUpper2=function(){
+			$scope.setUpper="has-background-info";
+			$scope.setUpper1="has-background-info";
+			$scope.setUpper2="has-background-warning";
+			getUpperIndex=0;
+		};
 	}
+	$scope.setLower="has-background-info";
+	$scope.setLower1="has-background-info";
+	$scope.setLower2="has-background-info";
 
 	function lowerCloth(){
-		$('.columns #lower').each( function(eq) {
-			$(this).off().click( function() {
-				//console.log(getLowerIndex);
-				$('.loHide').eq(getLowerIndex).hide();//上一件點選的褲子會從黃框變藍框
-				$('.loShow').eq(getLowerIndex).show();
-				window.setTimeout( function() {
-					$('.loShow').eq(eq).hide();//這次點選的的褲子會從藍框變黃框
-					$('.loHide').eq(eq).show();
-				});
-				getLowerIndex=eq;//抓取當前褲子的index
-			});
-		});
+		$scope.changeLower=function(){
+			$scope.setLower="has-background-warning";
+			$scope.setLower1="has-background-info";
+			$scope.setLower2="has-background-info";
+			getLowerIndex=0;
+		};
+		$scope.changeLower1=function(){
+			$scope.setLower="has-background-info";
+			$scope.setLower1="has-background-warning";
+			$scope.setLower2="has-background-info";
+			getLowerIndex=0;
+		};
+		$scope.changeLower2=function(){
+			$scope.setLower="has-background-info";
+			$scope.setLower1="has-background-info";
+			$scope.setLower2="has-background-warning";
+			getLowerIndex=0;
+		};
 	}
 
 	var button = document.querySelector('.is-success');
