e9ed0c72bd2fc82868225e182b56706c79acdbbf
DiuDiu <A8206115@g.pccu.edu.tw>
2022-08-20 17:05:13 +0800

    資料庫建置更新說明

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# 安裝sql 環境
+
+- 使用資料庫為 sql server
+- 使用工具不限
+
+## 操作步驟
+1. 建立資料庫 "intelligence_closet"
+2. 建立資料表，請在該資料庫的環境中執行　"在SQL這個資料夾中的 -> total.sql"
+3. 在 "DAO這個資料夾中 -> crudAccount.py" 裡面換成自己的帳號密碼(請注意往後這個檔案不要上傳到git)
+4. 並執行在最外層的 "crawlerStation.py" 這個檔案，自動抓取所有資料至station這個資料表中
--- SQL/create_color.sql
+++ SQL/create_color.sql
@@ -1,25 +1,53 @@
 drop table color
 
-create table color(
+create table color
+(
 
-	Id bigint PRIMARY KEY NOT NULL IDENTITY, -- 編號 ( 1, 2, 3 ... )
-	ColorEngName varchar(30)　NOT NULL, -- 顏色名稱(英文版本)
+	Id bigint PRIMARY KEY NOT NULL IDENTITY,-- 編號 ( 1, 2, 3 ... )
+	ColorEngName varchar(30) NOT NULL,-- 顏色名稱(英文版本)
 	ColorName nvarchar(30)-- 顏色名稱(中文版本)
 
 );
 
-select * from color
+select *
+from color
 
-insert color values ('Undefined', NULL) --1
-insert color values ('BLUE', '藍色') --2
-insert color values ('BLACK', '黑色') --3
-insert color values ('WHITE', '白色') --4
-insert color values ('RED', '紅色') --5
-insert color values ('ORANGE', '橘色') --6
-insert color values ('YELLOW', '黃色') --7
-insert color values ('GREEN', '綠色') --8
-insert color values ('PURPLE', '紫色') --9
-insert color values ('GRAY', '灰色') --10
-insert color values ('CREAMY WHITE','米白色') --11
-insert color values ('ARMY GREEN','軍綠色') --12
-insert color values ('KHAKI','卡其色') --13
+insert color
+values
+	('Undefined', NULL)--1
+insert color
+values
+	('BLUE', '藍色')--2
+insert color
+values
+	('BLACK', '黑色')--3
+insert color
+values
+	('WHITE', '白色')--4
+insert color
+values
+	('RED', '紅色')--5
+insert color
+values
+	('ORANGE', '橘色')--6
+insert color
+values
+	('YELLOW', '黃色')--7
+insert color
+values
+	('GREEN', '綠色')--8
+insert color
+values
+	('PURPLE', '紫色')--9
+insert color
+values
+	('GRAY', '灰色')--10
+insert color
+values
+	('CREAMY WHITE', '米白色')--11
+insert color
+values
+	('ARMY GREEN', '軍綠色')--12
+insert color
+values
+	('KHAKI', '卡其色')--13
