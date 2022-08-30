dcc21dcf3fd091e42e27c736efb67b3be201712e
moses0713 <58292166+moses0713@users.noreply.github.com>
2022-08-31 07:06:38 +0800

    updata: mui/putIn

--- View/mui/JS/PutIn.js
+++ View/mui/JS/PutIn.js
@@ -1,3 +1,4 @@
+
 function heartClick(a){
   if(a.style.color=="white")
     a.style.color="red";
@@ -5,20 +6,45 @@ function heartClick(a){
   a.style.color="white";
 }
 
-
-
-
-
-
-
-
-
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope) {
+  $scope.sssss='白色';
+  $scope.StartType = true;//開始介面bool
+  $scope.MainType = false;//主介面bool
 
+  $scope.putIn = [//資料
+    { 'pictures': './public/src/clothes_1.jpg','color': '白色', 'style': '連帽衫','like':5,'favorite':false}
+  ]
 
+  setInterval(function () {
+    $scope.$apply(function () {
 
+    });
+  }, 15);
+  
+  $scope.heartClicks = function () {
+    $scope.putIn.favorite=!$scope.putIn.favorite;
+  }
 
+  $scope.Start = function (){//開始辨識
+    $scope.StartType = false;
+    //TODO bootbox:1.為偵測到衣服，請放入衣服  2.辨識中，請稍等
+    $scope.MainType = true;
+  }
+
+  $scope.Back = function () {//退回衣服
+    $scope.MainType = false;
+  //TODO bootbox: 衣服退出中...
+    $scope.StartType = true;
+  }
+
+  $scope.Send = function () {//送出
+    $scope.MainType = false;
+  //TODO bootbox: 衣服收入中...
+    $scope.StartType = true;
+    console.log($scope.putIn)
+  //TODO 有個不知道是不是bug 我在DC問你
+  }
 
 
 });
