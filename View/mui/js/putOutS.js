f80d03591fb4e91344e019b60e8cf3b2c4e091d2
moses0713 <58292166+moses0713@users.noreply.github.com>
2022-09-03 01:44:03 +0800

    updata

--- View/mui/js/putOutS.js
+++ View/mui/js/putOutS.js
@@ -3,9 +3,24 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope) {
 
+  $scope.upSet=false;//上半身子選項
+  $scope.downSet=false;//下半身子選項
+  $scope.otherSet=false;//其他子選項
 
-
-
-
+  $scope.upClick = function () {
+    $scope.upSet=!$scope.upSet;
+    $scope.downSet=false;
+    $scope.otherSet=false;
+  }
+  $scope.downClick = function () {
+    $scope.upSet=false;
+    $scope.downSet=!$scope.downSet;
+    $scope.otherSet=false;
+  }
+  $scope.otherClick = function () {
+    $scope.upSet=false;
+    $scope.downSet=false;
+    $scope.otherSet=!$scope.otherSet;
+  }
 
 });
