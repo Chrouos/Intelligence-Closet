d7561c07d55ff40cb8181163680c7e67b51a43e5
DiuDiu <A8206115@g.pccu.edu.tw>
2022-09-03 02:50:00 +0800

    update: sql sub_category

--- SQL/create_sub_category.sql
+++ SQL/create_sub_category.sql
@@ -9,54 +9,38 @@ create table sub_category
 	Name nvarchar(255)-- 衣物名稱
 );
 
-
 INSERT INTO sub_category
-VALUES(4, 'Blazer', 3, '西裝外套')
-
+VALUES(1, 'T-Shirt', 1, 'T恤')
 INSERT INTO sub_category
-VALUES(3, 'Dress', 3, '洋裝')
-
+VALUES(1, 'Undershirt', 1, '背心')
 INSERT INTO sub_category
-VALUES(1, 'Top', 2, '上半身')
-
+VALUES(1, 'Shirt', 1, '襯衫')
 INSERT INTO sub_category
-VALUES(6, 'Hat', 1, '帽子')
-
-INSERT INTO sub_category
-VALUES(1, 'Hoodie', 4, '連帽衫')
-
+VALUES(1, 'Polo', 1, '運動衫')
 INSERT INTO sub_category
 VALUES(1, 'Longsleeve', 3, '長袖')
-
 INSERT INTO sub_category
-VALUES(4, 'Outwear', 3, '外套')
-
+VALUES(1, 'Hoodie', 4, '帽T')
 INSERT INTO sub_category
-VALUES(2, 'Pants', 3, '褲子')
-
-INSERT INTO sub_category
-VALUES(1, 'Polo', 2, 'Polo衫')
-
-INSERT INTO sub_category
-VALUES(1, 'Shirt', 2, '襯衫')
-
-INSERT INTO sub_category
-VALUES(7, 'Shoes', 1, '鞋')
+VALUES(1, 'Outwear', 4, '外套')
 
 INSERT INTO sub_category
 VALUES(2, 'Shorts', 1, '短褲')
+INSERT INTO sub_category
+VALUES(2, 'Pants', 3, '長褲')
+INSERT INTO sub_category
+VALUES(2, 'Skirt', 2, '裙子')
 
 INSERT INTO sub_category
-VALUES(2, 'Skirt', 1, '裙子')
+VALUES(3, 'Dress', 3, '洋裝')
+INSERT INTO sub_category
+VALUES(3, 'Body', 2, '連身衣')
 
 INSERT INTO sub_category
-VALUES(1, 'T-Shirt', 2, 'T恤')
+VALUES(4, 'Blazer', 4, '西裝外套')
 
 INSERT INTO sub_category
-VALUES(1, 'Undershirt', 1, 'T恤')
-
-INSERT INTO sub_category
-VALUES(8, 'Not_sure', 0, Null)
+VALUES(8, 'Not_sure', 0, '不確定')
 
 
 select *
