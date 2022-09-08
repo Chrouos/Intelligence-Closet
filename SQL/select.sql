d2f0069444d2aace0b3026df029380fff6c69108
moses0713 <58292166+moses0713@users.noreply.github.com>
2022-09-08 17:40:01 +0800

    updata: UI分類選單

--- SQL/select.sql
+++ SQL/select.sql
@@ -1,29 +1,27 @@
+--使用者相關
 
+--衣服資訊相關
+select * from clothes_node
+select * from category
+select * from color
+select * from sub_category
+--推薦相關
+select * from color_graph
+select * from clothes_graph
+select * from user_combs
+select * from user_dashboard
+--天氣相關
+select * from city
+select * from station
 
-select *
-from v_station;
+--show
 
-select *
-from v_clothes_information;
+select * from v_category_clothes
 
-select *
-from v_clothes_graph_edge;
+select * from v_clothes_graph
 
-select *
-from v_category_clothes;
+select * from v_clothes_node
 
-select *
-from category;
+select * from v_color_graph
 
-SELECT *
-FROM clothes_information;
-
-
-select *
-from sub_category;
-
-select *
-from color;
-
-
-
+select * from v_station
